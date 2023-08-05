144142e6b90400cdfc48c8f20ed859a4d2933764
Tatsu <nekonekokoneko788@gmail.com>
2023-08-06 08:02:05 +0900

    modify and add html css

--- js/common.js
+++ js/common.js
@@ -41,6 +41,11 @@ $(function() {
             {
                 breakpoint: 769,
                 settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+                breakpoint: 480,
+                settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1,
                 },
