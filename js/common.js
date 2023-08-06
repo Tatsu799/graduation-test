c6ab3d069e8571497cdaabe3e87b7c741280c24d
Tatsu <nekonekokoneko788@gmail.com>
2023-08-06 19:21:15 +0900

    modify index.html css

--- js/common.js
+++ js/common.js
@@ -1,5 +1,16 @@
 'use strict';
 
+let startPos = 0, winScrollTop = 0;
+$(window).on('scroll', function () {
+    winScrollTop = $(this).scrollTop();
+    if (winScrollTop >= startPos) {
+        $('.header').addClass('hide');
+    } else {
+        $('.header').removeClass('hide');
+    }
+    startPos = winScrollTop;
+});
+
 let windowSize = $(window).width();
 $(function () {
     if (windowSize > 769) {
