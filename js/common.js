b6e2bea1ee78e534d17933d3bbe17d90b88eddd2
Tatsu <nekonekokoneko788@gmail.com>
2023-08-06 20:19:02 +0900

    modify html

--- js/common.js
+++ js/common.js
@@ -1,6 +1,17 @@
 'use strict';
 
-let startPos = 0, winScrollTop = 0;
+const loadAnime = $('.spinner-box');
+$(window).on('load', function() {
+    loadAnime.delay(700).fadeOut(1000);
+});
+
+function stopLoad() {
+    loadAnime.delay(500).fadeOut(700);
+}
+setTimeout('stopLoad()', 10000);
+
+let startPos = 0
+let winScrollTop = 0;
 $(window).on('scroll', function () {
     winScrollTop = $(this).scrollTop();
     if (winScrollTop >= startPos) {
@@ -25,16 +36,6 @@ $(function () {
     });
     }
 });
-
-const loadAnime = $('.spinner-box');
-$(window).on('load', function() {
-    loadAnime.delay(700).fadeOut(1000);
-});
-
-function stopLoad() {
-    loadAnime.delay(10000).fadeOut(1000);
-}
-setTimeout('stopLoad()', 20000);
 
 // slider 
 $(function() {
